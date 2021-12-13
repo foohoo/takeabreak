852b5a33a0467e8c44f7033caa79bdaac452edc1
Andrei Cartan <andrei.cartan@gmail.com>
2021-12-13 15:16:31 +0000

    Set random background

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	export let name: string;
+	let root = document.querySelector(":root");
+	function handleClick() {
+		// root.setAttribute('--back-url', `url(https://source.unsplash.com/random/1280x720/?landscape)`)
+		root.setAttribute('--back-url', `url(https://source.unsplash.com/random/1280x720/?nature,landscape)`)
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={() => handleClick()}>Button</button>
 </main>
 
 <style>
@@ -20,6 +26,16 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	:root {
+		--back-url: url(https://source.unsplash.com/random/1280x720/?cat);
+	}
+
+	:global(body) {
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-image: var(--back-url);
 	}
 
 	@media (min-width: 640px) {
