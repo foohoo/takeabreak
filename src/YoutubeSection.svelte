ab86de168e4fe7b44e95e1c506776d646f950221
dexterbarnes <dbarnes@qmetric.co.uk>
2021-12-14 17:00:48 +0000

    Adding static video and making it only 1

--- src/YoutubeSection.svelte
+++ src/YoutubeSection.svelte
@@ -3,9 +3,9 @@
     import gapi from 'gapi-client';
     $: videoResponses = [];
 
-    const options = {
-    height: '100',
-    width: '100'
+    let options = {
+    height: '500',
+    width: '900'
   };
 
     export let hidden = false;
@@ -13,23 +13,19 @@
     function onClientLoad() {
         gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
     }
-    // Called automatically when YouTube API interface is loaded (see line 9).
+
     function onYouTubeApiLoad() {
         gapi.client.setApiKey('AIzaSyAGyePXAypEPUysjmvVpu4HByxCJu4byEU');
         search();
     }
 
-    // Called when the search button is clicked in the html code
     function search() {
-        // Use the JavaScript client library to create a search.list() API call.
         var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q:"cat"
     });
-        // Send the request to the API server, call the onSearchResponse function when the data is returned
         // request.execute(onSearchResponse);
     }
-    // Triggered by this line: request.execute(onSearchResponse);
     function onSearchResponse(response) {
         response.result.items.forEach(video => videoResponses.push(video.id.videoId));
         videoResponses = videoResponses;
@@ -39,16 +35,17 @@
     gapi.load('client', onClientLoad);
 </script>
 
-{#if hidden}
+{#if !hidden}
     <div class="videoBlock">
-        {#each videoResponses as id}
-            <YouTube videoId={id} {options} />
-        {/each}
+        <!-- {#each videoResponses as id} -->
+            <YouTube videoId="xNyhuMxyjdw" {options} />
+        <!-- {/each} -->
     </div>
 {/if}
 
 <style>
     .videoBlock {
         display: inline;
+        text-align: center;
     }
 </style>
